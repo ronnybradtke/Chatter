00d689da3fba31eb9eb26e5ed8f375f2edf0e0f2
unknown <ronny.bradtke@gmail.com>
2015-09-12 10:50:07 -0500

    fixing README.md description

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # chatter
 
-FIXME
+Ronny's fun time chat app
 
 ## Prerequisites
 
@@ -16,4 +16,4 @@ To start a web server for the application, run:
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 RCB
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ronny's fun time chat app"
+  :url "http://example.com/RCB"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
